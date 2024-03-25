5dc902f0000e7a4216ed7957d426ee8ee02650f8
FinotoZx <118355565+FinotoZx@users.noreply.github.com>
2024-03-25 09:20:51 -0400

    Update Estudante.tsx

--- src/componentes/Estudante.tsx
+++ src/componentes/Estudante.tsx
@@ -3,8 +3,8 @@ function Estudante() {
     return (
         <div className='box-estudante'>
                 <img src="https://github.com/FinotoZx.png" alt="Foto Estudante" />
-                <h3 className='nome-estudante'>João Victor da Silva Caetano</h3>
-                <p className='descricao-estudante'>Estudante do IFMS nas matérias de Desenvolvimento - Desenvolvimento Web</p>
+                <h3 className='nome-estudante'>Guilherme Finoto Viana</h3>
+                <p className='descricao-estudante'>Estudante do IFMS</p>
                 <a href="https://github.com/FinotoZx">GitHub</a>
             </div>
     )
